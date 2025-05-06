701c4b54634b8afbf4bfe30d48329b6b1116a853
h-nakashima <h-nakashima@sofnet-pro.co.jp>
2025-05-06 17:33:35 +0900

    test-1-2

---
+++ src/app/test/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from "@/components/ui/button";
+
+const Test = () => {
+  return (
+    <div>
+      <div className="mt-8">
+        <Button>ボタン</Button>
+      </div>
+    </div>
+  );
+};
+
+export default Test;
