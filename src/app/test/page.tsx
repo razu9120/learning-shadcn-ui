d14185b35a31957c759a6c6c7dfcbd5b7791484c
h-nakashima <h-nakashima@sofnet-pro.co.jp>
2025-05-06 17:04:19 +0900

    adding button component

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -9,6 +9,9 @@ const Test = () => {
       <div className="mt-8">
         <Button>ボタン</Button>
       </div>
+      <div className="mt-8">
+        <Button>ボタン</Button>
+      </div>
     </div>
   );
 };
