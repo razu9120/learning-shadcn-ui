f953c7efa49a980b9796647b7f731763ea1da8d9
h-nakashima <h-nakashima@sofnet-pro.co.jp>
2025-05-06 18:01:53 +0900

    test

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -13,6 +13,9 @@ const Test = () => {
       <div className="mt-8">
         <Button>ボタン</Button>
       </div>
+      <div className="mt-8">
+        <Button>ボタン</Button>
+      </div>
     </div>
   );
 };
